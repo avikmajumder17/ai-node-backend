733422f2c5bb1cae320051727d7fafcadfbcc3d5
avikmajumder17 <avikmajumder17@gmail.com>
2026-08-21 00:11:08 +0530

    first commit

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const app = express();
 
 app.use(cors({
     origin: [
-        "http://localhost:5173",
-        "http://localhost:5174"
+        // "http://localhost:5173",
+        // "http://localhost:5174",
+        "https://ai-vite-project.netlify.app",
+        "https://ai-vite-admin.netlify.app"
     ]
 }))
 
