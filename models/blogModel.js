d92c969f6e38fcfa1d0dfd800d969310345c4a6c
avikmajumder17 <avikmajumder17@gmail.com>
2026-08-24 00:59:48 +0530

    first commit

--- models/blogModel.js
+++ models/blogModel.js
@@ -5,29 +5,29 @@ const mongoose = require("mongoose");
 const blogSchema = new mongoose.Schema({
     image: {
         type: String,
-        required: [true, "A blog must have an image"]
+        required: [false, "A blog must have an image"]
     },
     blogCategory: {
         type: String,
-        required: [true, "A blog must have a category"]
+        required: [false, "A blog must have a category"]
     },
     blogTitle: {
         type: String,
-        required: [true, "A blog must have a title"]
+        required: [false, "A blog must have a title"]
     },
     blogPostDate: {
         type: Date,
         default: new Date,
-        required: [true, "A blog must have a post date"]
+        required: [false, "A blog must have a post date"]
     },
     blogDescription: {
         type: String,
-        required: [true, "A blog must have a description"]
+        required: [false, "A blog must have a description"]
     },
-    blogKeyTakeways: {
-        type: [String],
-        required: [true, " A blog must have a category"]
-    }
+    // blogKeyTakeways: {
+    //     type: [String],
+    //     required: [false, " A blog must have a category"]
+    // }
 });
 
 
